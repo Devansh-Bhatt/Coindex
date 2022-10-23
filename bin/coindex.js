26686d0b09c68d3822928c78d2ebe98be4590b65
Devansh-Bhatt <devanshbhatt61135@gmail.com>
2022-10-24 00:44:33 +0530

    Commit 2

--- bin/coindex.js
+++ bin/coindex.js
@@ -5,7 +5,7 @@ const program = require('commander');
 const pkg = require('../package.json');
 program
     .version(pkg.version)
-    .command('key','Manage API key --https://nomics.com')
+    .command('key','Manage API key --https://coinlayer.com')
     .command('check','Check coin price info')
     .parse(process.argv);
 
